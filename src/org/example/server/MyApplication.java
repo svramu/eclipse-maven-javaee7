9393cddeb68f89dee2fae1781e475f7ef7c5c39a
S V Ramu <svramu@gmail.com>
2015-04-17 17:39:31 +0530

    spell mistake corrected

--- src/org/example/server/MyApplication.java
+++ src/org/example/server/MyApplication.java
@@ -3,7 +3,7 @@ package org.example.server;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-//somme comment to create conflict
+//some comment to create conflict
 @ApplicationPath("/rest")
 public class MyApplication extends Application {
 

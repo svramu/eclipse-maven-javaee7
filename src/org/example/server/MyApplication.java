96f2cf1dfbd8f7acbe04eb7356b029fbd719fe63
S V Ramu <svramu@gmail.com>
2015-04-26 21:41:04 +0530

    the rest prefix removed, and seperate packages created for each noun. TBD: model many-to-many between employee-region

--- src/org/example/server/MyApplication.java
+++ src/org/example/server/MyApplication.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 //some comment to create conflict
-@ApplicationPath("/rest")
+@ApplicationPath("/")
 public class MyApplication extends Application {
 
 }

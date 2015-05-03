e58e66307af881df7a8525755a828e996ff22787
S V Ramu <svramu@gmail.com>
2015-05-03 19:15:16 +0530

    merged monolith_try

---
+++ src/org/example/server/Farmer_.java
@@ -0,0 +1,12 @@
+package org.example.server;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-05-03T19:13:26.373+0530")
+@StaticMetamodel(Farmer.class)
+public class Farmer_ {
+	public static volatile SingularAttribute<Farmer, Integer> id;
+	public static volatile SingularAttribute<Farmer, String> name;
+}
